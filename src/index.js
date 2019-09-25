e6200bf61312279fc648d741cc0f316f31da6fdc
mrinex <mrinex@mail.ru>
2019-09-25 17:19:14 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,98 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+        
+        function fooo(array) {
+          let word ='';
+          let regex = /[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/g;
+          let r = /^0+/g;
+            array = array.match(regex);
+            for(j = 0; j < array.length; j++)
+            {
+              array[j] = array[j].replace(r,'');
+              array[j] = array[j].replace(/11/g, '-');
+              array[j] = array[j].replace(/10/g,'.');
+              word += MORSE_TABLE[array[j]];
+            }
+            return word+' ';
+        }
+    //function ----------------------------------------------
+        mainwords = expr.match(/[0-9]+/g);
+        let answer = '';
+        for(i = 0; i < mainwords.length; i++)
+        {
+            answer += fooo(mainwords[i]);
+        }
+        answer = answer.replace(/ $/, '');
+        return answer;
+    
+    /*
+    function fooo(array) {
+        let word ='';
+      let regex = /[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/g;
+      let r = /^0+/g;
+      for(i = 0;i < array.length; i++)
+      {
+        array[i] = array[i].match(regex);
+        alert('i='+array[i]);
+        for(j = 0; j < array[i].length; j++)
+        {
+          alert('ij='+array[i][j]);
+          array[i][j] = array[i][j].replace(r,'');
+            array[i][j] = array[i][j].replace(/11/g, '-');
+          array[i][j] = array[i][j].replace(/10/g,'.');
+          word += MORSE_TABLE[array[i][j]];
+        }
+        alert('str='+word)
+        return word;
+      }
+  }
+ 
+
+    let word = [];
+    let array = [];
+    array = expr.replace('**********', ' ');
+    array = array.split(' ');
+    for(i = 0; i < array.length; i++)
+    {
+    	word[i] = (array[i].length)/10;
+    }
+
+    let regex = /[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/g;
+	let r = /^0+/g;	
+	let code = expr.match(regex);
+	let arr ='';
+	let nospace ='';
+
+	for(i = 0;i < code.length; i++)
+    {
+    	arr = code[i].replace(r,'');
+      	arr = arr.replace(/11/g, '-');
+        arr = arr.replace(/10/g,'.');
+        nospace += MORSE_TABLE[arr];
+    }
+    return nospace; 
+
+// new version -------------------------------------------------------
+    
+    let array = [];
+	array = expr.replace('**********', ' ');
+	array = array.split(' ');
+	let regex = /[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/g;
+	let r = /^0+/g;
+	for(i = 0;i < array.length; i++)
+    {
+      array[i] = array[i].match(regex);
+      for(j = 0; j < array[i].length; j++)
+      {
+    	array[i][j] = array[i][j].replace(r,'');
+      	array[i][j] = array[i][j].replace(/11/g, '-');
+        array[i][j] = array[i][j].replace(/10/g,'.');
+        array[i][j] += MORSE_TABLE[array[i][j]];
+      }
+    }
+    return array;
+    */
 }
 
 module.exports = {
